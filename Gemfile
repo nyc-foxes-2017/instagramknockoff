889f7079788191f136c47cb35dc66e9081cafff6
Michael Benson <email": null>
2017-03-27 17:20:22 -0400

    deletes

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
+gem "paperclip", "~> 5.0.0"
+gem 'simple_form'
+gem 'bootstrap-sass', '~> 3.3.6'
 
 
 # Use jquery as the JavaScript library
@@ -27,7 +30,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'haml'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+
+before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if @post = Post.create(post_params)
+      flash[:success] ="Your post has been created"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "your post couldnt be created"
+      render:new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post Updated"
+      redirect_to(post_path(@post))
+    else
+      flash.now[:alert] = "update failed"
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "your post has been deleted"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
